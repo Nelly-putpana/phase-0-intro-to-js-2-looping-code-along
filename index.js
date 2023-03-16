a882db4a51434bbc9a75161f5c260ae3245d6d2e
Nelly Putpana <nelly.putpana@gmail.com>
2023-03-16 17:53:33 -0600

    looping laborat

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards (names, event)
+{const Messages = []; 
+    for (let i= 0; i < names.length; i++) {
+Messages.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);}
+return Messages;}
+
+
+
+function countDown(num) {while (num > 0) {
+    console.log(num);
+    num--;
+} console.log(num);}
